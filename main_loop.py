6d19460950c75b8ac8935c97eaf6091676eac6dc
HarryJordan <>
2024-02-09 12:09:25 +0000

    Adding some data

--- main_loop.py
+++ main_loop.py
@@ -1,41 +1,48 @@
 import time
 import queue
 
-from .events import MarketEvent, SignalEvent, OrderEvent, FillEvent
+from datetime import datetime
+from event import MarketEvent, SignalEvent, OrderEvent, FillEvent
+from data import HistoricCSVDataHandler
+from strategy import BuyAndHoldStrategy
+from portfolio import NaivePortfolio
+from execution import SimulatedExecutionHandler
 
-
-from .data import HistoricCSVDataHandler
-from .strategy import BuyAndHoldStrategy
-from .portfolio import Portfolio
-from .broker import ExecutionHandler
-
-event_queue = Queue.Queue()
-data = HistoricCSVDataHandler()
-strategy = BuyAndHoldStrategy()
-portfolio = Portfolio()
-broker = ExecutionHandler()
-
-while True:
-    if data.continue_backtest is True:
-        data.update_latest_data()
-    else:
-        break
-
+def backtest(events, data, portfolio, strategy, broker):
     while True:
-        try:
-            event = event_queue.get(block=False)
-        except Queue.Empty:
+        data.update_bars()
+        if data.continue_backtest == False:
             break
+            
+        while True:
+            try:
+                event = events.get(block=False)
+            except queue.Empty:
+                break
+            
+            if event is not None:
+                if event.type == "MARKET":
+                    strategy.calculate_signals(event)
+                    portfolio.update_timeindex(event)
+                elif event.type == "SIGNAL":
+                    portfolio.update_signal(event)
+                elif event.type == "ORDER":
+                    broker.execute_order(event)
+                elif event.type == "FILL":
+                    portfolio.update_fill(event)
+                    
+    stats = portfolio.output_summary_stats()
+    
+    for stat in stats:
+        print(stat[0] + ": " stat[1])
+        
+events = queue.Queue()
+data = HistoricCSVDataHandler(events, 'csv/', ['OMXS30'], DataSource.NASDAQ)
+portfolio = NaivePortfolio(data, events, '', initial_capital=2000)
+strategy = MovingAveragesLongStrategy(data, events, portfolio, 50, 100, version=1)
+portfolio.strategy_name = strategy.name
+broker = SimulateExecutionHandler(events)
 
-        if event is not None:
-            if isinstance(event, MarketEvent):
-                strategy.calculate_signals(event)
-                portfolio.update_timeindex(event)
-            elif isinstance(event, SignalEvent):
-                portfolio.update_signal(event)
-            elif isinstance(event, OrderEvent):
-                broker.execute_order(event)
-            elif isinstance(event, FillEvent):
-                portfolio.update_fill(event)
-
-    time.sleep(10 * 60)
+backtest(events, data, portfolio, strategy, broker)
+    
+            
